5193437f3c0b8fcc87b42e2f33e6bbe6420e6110
Alicia Thoney <alicia.thoney@ucf.edu>
2025-04-21 13:36:28 -0400

    update config for deploy

--- doodle-net-docs/docusaurus.config.js
+++ doodle-net-docs/docusaurus.config.js
@@ -15,15 +15,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/doodle-net/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'athoney', // Usually your GitHub org/user name.
   projectName: 'doodle-net', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
